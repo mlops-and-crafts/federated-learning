2a1295d4501a050dadb04363a745888323557881
Oege Dijk <oege.dijk@fourkind.com>
2023-06-14 16:05:56 +0200

    dashboard cleanup

--- federated/dashboard.py
+++ federated/dashboard.py
@@ -1,46 +1,58 @@
-import dash
-from dash import html
-from dash import dcc, dash_table
+import re
+from pathlib import Path
+from typing import Union, List, Dict
+
 import numpy as np
+import pandas as pd
+
+from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
-import pandas as pd
-import re
+
 import cfg
 
 
-def parse_server_metrics_from_log(fp):
+def parse_server_metrics_from_log(server_log_path:Union[str, Path]) -> pd.DataFrame:
     result_dict = dict()
-    with open(fp, 'r') as f:
+    with open(Path(server_log_path), 'r') as f:
         server_rmse_lines = [x for x in f.readlines() if 'SERVER Round' in x]
         for line in server_rmse_lines:
-            m = re.match('SERVER Round (.*) RMSE: (.*) R\^2: (.*) coefs = .*', line)
-            result_dict[int(m.group(1))] = {
-                'RMSE': float(m.group(2)), 'R-squared': float(m.group(3))
-                }
-    result = pd.DataFrame.from_dict(result_dict, orient='index')
-    return result
+            regexp_result = re.match('SERVER Round (.*) RMSE: (.*) R\^2: (.*) coefs = .*', line)
+            result_dict[int(regexp_result.group(1))] = {
+                'RMSE': float(regexp_result.group(2)), 
+                'R-squared': float(regexp_result.group(3))
+            }
+    result_df = pd.DataFrame.from_dict(result_dict, orient='index')
+    return result_df
 
-fp = f'{cfg.LOGFILE_DIR}/server.log'
-app = dash.Dash(__name__, update_title=None)
-loss_data = parse_server_metrics_from_log(fp)
-figure = px.line(loss_data, x=loss_data.index, y="RMSE", title='Federated loss')
-graph = dcc.Graph(id='live-update-graph', figure=figure)
-update_interval =  dcc.Interval(
-            id='interval-component1',
-            interval=1000, # in milliseconds
-            n_intervals=0
-        )
-div1 = html.Div([graph, update_interval])
-app.layout = html.Div([div1])
+server_log_path = Path(f'{cfg.LOGFILE_DIR}/server.log')
+loss_data = parse_server_metrics_from_log(server_log_path)
+
+app = Dash(__name__)
+app.title = "Federated Learning Dashboard"
+app.layout = html.Div([
+    html.Div([
+        dcc.Graph(id='federated-rmse-graph'),
+    ]),
+    dcc.Interval(
+        id='interval-ticker',
+        interval=1000, # in milliseconds
+        n_intervals=0
+    )
+])
 
 @app.callback(
-        Output('live-update-graph', component_property='figure'), 
-        Input('interval-component1', 'n_intervals')
+        Output('federated-rmse-graph', 'figure'), 
+        Input('interval-ticker', 'n_intervals')
 )
 def update_data(n_intervals):
-    data = parse_server_metrics_from_log(fp)
-    figure = px.line(data, x=data.index, y="RMSE", title='Federated loss')
+    metrics_df = parse_server_metrics_from_log(server_log_path)
+    figure = px.line(
+        metrics_df, 
+        x=metrics_df.index, 
+        y="RMSE", 
+        title='Federated loss'
+    )
     return figure
 
 if __name__ == "__main__":
