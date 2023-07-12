3c8a9243b6952924513125b57d4224400a9724b4
Oege Dijk <oege.dijk@fourkind.com>
2023-07-12 20:28:32 +0200

    deleted old client, server and workshop folders

--- client/client_config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-RETRY_SLEEP_TIME = os.environ.get("CLIENT_RETRY_SLEEP_TIME", 10)
-SERVER_ADDRESS = os.environ.get("SERVER_ADDRESS", "localhost")
-SERVER_PORT = os.environ.get("SERVER_PORT", "8080")
